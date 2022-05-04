83b7ddc58527fa716e1ca2014d53b6fdfb13c88e
11何紹榕 <90734805+Vru0811@users.noreply.github.com>
2022-05-04 22:04:30 +0800

    triangle.c
    lll

---
+++ triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main () {
+
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        if (i%2== ){
+        for (int j = 0; j < n-i; j++) {
+             printf("*");
+        } eise {    
+            printf("+");
+        }
+        printf("\n");
+    }
+}
